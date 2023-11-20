3fc1684d65b1982453b5f5241a612a1b663b66f9
woodong-222 <74187674+woodong-222@users.noreply.github.com>
2023-11-20 15:58:34 +0900

    동우+은섭 작업 합친거

--- foodmap/src/foodmap/Foodmap.java
+++ foodmap/src/foodmap/Foodmap.java
@@ -1,7 +1,6 @@
 package foodmap;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -12,16 +11,21 @@ import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.Timer;
 
-public class Foodmap extends JFrame {
 
+public class Foodmap extends JFrame {
+	
+	String address; // 주소 변수 (여기에 주소 넣으면 지도에서 이 주소값을 불러옴)
+	JLabel map; // 지도 사진 (여기에서 지도 출력)
+	
 	private Image screenImage;
 	private Graphics screenGraphic;
 
 	// 로딩, 맵 경로 지정(맵은 바뀔 가능성 많음)
-	private Image Background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
-	private Image Map = new ImageIcon(Main.class.getResource("../images/Map1.jpg")).getImage(); // 제대로된 지도 이미지 교체 필요
+	private Image Background = new ImageIcon(Main.class.getResource("../images/introBackground.png")).getImage();
+	// private Image Map = new ImageIcon(Main.class.getResource("../images/Map1.jpg")).getImage(); // 제대로된 지도 이미지 교체 필요
 
 	// 버튼 이미지 경로 지정
 	private ImageIcon homeEnteredbutton = new ImageIcon(Main.class.getResource("../images/home2.png"));
@@ -38,6 +42,10 @@ public class Foodmap extends JFrame {
 	private ImageIcon WfoodBasicbutton = new ImageIcon(Main.class.getResource("../images/Wfood.png"));
 	private ImageIcon SfoodEnteredbutton = new ImageIcon(Main.class.getResource("../images/Sfood2.png"));
 	private ImageIcon SfoodBasicbutton = new ImageIcon(Main.class.getResource("../images/Sfood.png"));
+	private ImageIcon PEnteredbutton = new ImageIcon(Main.class.getResource("../images/plu2.png"));
+	private ImageIcon PBasicbutton = new ImageIcon(Main.class.getResource("../images/plu.png"));
+	private ImageIcon MEnteredbutton = new ImageIcon(Main.class.getResource("../images/min2.png"));
+	private ImageIcon MBasicbutton = new ImageIcon(Main.class.getResource("../images/min.png"));
 
 	// 버튼 생성
 	private JButton homebutton = new JButton(homeBasicbutton); // 홈버튼 생성
@@ -47,10 +55,10 @@ public class Foodmap extends JFrame {
 	private JButton Jfoodbutton = new JButton(JfoodBasicbutton);
 	private JButton Wfoodbutton = new JButton(WfoodBasicbutton);
 	private JButton Sfoodbutton = new JButton(SfoodBasicbutton);
+	private JButton Pbutton = new JButton(PBasicbutton);
+	private JButton Mbutton = new JButton(MBasicbutton);
 
 	private boolean isScreen1 = false; // 메인화면이면 true
-
-	private int mouseX, mouseY; // 마우스 위치
 
 	public Foodmap() {
 		setUndecorated(true); // 메뉴바 같은거 안보이게인데 이거 보이게 하고 싶어
@@ -64,6 +72,9 @@ public class Foodmap extends JFrame {
 		setBackground(new Color(0, 0, 0, 0)); // 배경 투명색
 		setLayout(null);
 
+        // NaverMap 객체 생성
+        NaverMap naverMap = new NaverMap(this);
+        
 		Timer timer = new Timer(3000, new ActionListener() { // 3초뒤에 로딩화면 숨김
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -74,7 +85,7 @@ public class Foodmap extends JFrame {
 		});
 
 		// 홈 버튼
-		homebutton.setBounds(1075, 50, 65, 65); // 버튼 위치 및 사이즈
+		homebutton.setBounds(900, 20, 65, 65); // 버튼 위치 및 사이즈
 		homebutton.setBorderPainted(false);
 		homebutton.setContentAreaFilled(false);
 		homebutton.setFocusPainted(false);
@@ -96,7 +107,7 @@ public class Foodmap extends JFrame {
 		});
 
 		// 찜 버튼
-		likedbutton.setBounds(1150, 50, 65, 65); // 버튼 위치 및 사이즈
+		likedbutton.setBounds(975, 20, 65, 65); // 버튼 위치 및 사이즈
 		likedbutton.setBorderPainted(false);
 		likedbutton.setContentAreaFilled(false);
 		likedbutton.setFocusPainted(false);
@@ -118,7 +129,7 @@ public class Foodmap extends JFrame {
 		});
 
 		// 한식 버튼
-		Kfoodbutton.setBounds(0, 0, 360, 144); // 버튼 위치 및 사이즈
+		Kfoodbutton.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
 		Kfoodbutton.setBorderPainted(false);
 		Kfoodbutton.setContentAreaFilled(false);
 		Kfoodbutton.setFocusPainted(false);
@@ -138,8 +149,8 @@ public class Foodmap extends JFrame {
 		 	} 
 		});
 		
-		// 중식 버튼
-		Cfoodbutton.setBounds(0, 144, 360, 144);
+		// 중식 버튼 
+		Cfoodbutton.setBounds(0, 100, 360, 100);
 		Cfoodbutton.setBorderPainted(false);
 		Cfoodbutton.setContentAreaFilled(false);
 		Cfoodbutton.setFocusPainted(false);
@@ -160,7 +171,7 @@ public class Foodmap extends JFrame {
 		});
 		
 		// 일식 버튼
-		Jfoodbutton.setBounds(0, 288, 360, 144);
+		Jfoodbutton.setBounds(0, 200, 360, 100);
 		Jfoodbutton.setBorderPainted(false);
 		Jfoodbutton.setContentAreaFilled(false);
 		Jfoodbutton.setFocusPainted(false);
@@ -181,7 +192,7 @@ public class Foodmap extends JFrame {
 		});
 		
 		//양식 버튼
-		Wfoodbutton.setBounds(0, 432, 360, 144);
+		Wfoodbutton.setBounds(0, 300, 360, 100);
 		Wfoodbutton.setBorderPainted(false);
 		Wfoodbutton.setContentAreaFilled(false);
 		Wfoodbutton.setFocusPainted(false);
@@ -202,7 +213,7 @@ public class Foodmap extends JFrame {
 		});
 		
 		// 분식 버튼
-		Sfoodbutton.setBounds(0, 576, 360, 144);
+		Sfoodbutton.setBounds(0, 400, 360, 100);
 		Sfoodbutton.setBorderPainted(false);
 		Sfoodbutton.setContentAreaFilled(false);
 		Sfoodbutton.setFocusPainted(false);
@@ -221,13 +232,71 @@ public class Foodmap extends JFrame {
 			 // 분식 버튼 눌렀을 때 
 		 	} 
 		});
+		
+		// + 버튼
+				Pbutton.setBounds(1020, 460, 32, 32);
+				Pbutton.setBorderPainted(false);
+				Pbutton.setContentAreaFilled(false);
+				Pbutton.setFocusPainted(false);
+				Pbutton.addMouseListener(new MouseAdapter() {
+					@Override 
+					public void mouseEntered(MouseEvent e) { // 마우스 가까이 가면
+						Pbutton.setIcon(PEnteredbutton); 
+					}
+				 
+					@Override 
+					public void mouseExited(MouseEvent e) { // 마우스 멀어지면
+						Pbutton.setIcon(PBasicbutton); 
+					}
+				 
+				 @Override public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+					 NaverMap.mapsize++; // 맵 사이즈 늘리기
+					 naverMap.actionPerformed(null); // 지도 새로고침
+				 	} 
+				});
+		// - 버튼
+				Mbutton.setBounds(980, 460, 32, 32);
+				Mbutton.setBorderPainted(false);
+				Mbutton.setContentAreaFilled(false);
+				Mbutton.setFocusPainted(false);
+				Mbutton.addMouseListener(new MouseAdapter() {
+					@Override 
+					public void mouseEntered(MouseEvent e) { // 마우스 가까이 가면
+						Mbutton.setIcon(MEnteredbutton); 
+					}
+				 
+					@Override 
+					public void mouseExited(MouseEvent e) { // 마우스 멀어지면
+						Mbutton.setIcon(MBasicbutton); 
+					}
+				 
+				 @Override public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+					 NaverMap.mapsize--; // 맵 사이즈 줄이기
+					 naverMap.actionPerformed(null); // 지도 새로고침
+				 	} 
+				});
+		
+        JFrame frm = new JFrame();
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Container c = frm.getContentPane();
 
 		timer.setRepeats(false); // 한 번만 실행
 		timer.start();
+		
+		// 지도 관련
+        address = "충절로 1600"; // 도로명 주소를 집어넣음
+        map = new JLabel(); // 지도 객체 생성
+        map.setBounds(360, -111, 877, 720); // 지도 위치 설정
+
+
+        // 지도 새로고침
+        naverMap.actionPerformed(null);
+
 	}
 
 	public void setScreen1() { // 스크린 1
 		isScreen1 = true;
+		// 버튼들
 		add(homebutton);
 		add(likedbutton);
 		add(Kfoodbutton);
@@ -235,6 +304,9 @@ public class Foodmap extends JFrame {
 		add(Jfoodbutton);
 		add(Wfoodbutton);
 		add(Sfoodbutton);
+		add(Pbutton);
+		add(Mbutton);
+		add(map); // 지도
 	}
 
 	public void paint(Graphics g) {
@@ -247,7 +319,7 @@ public class Foodmap extends JFrame {
 	public void screenDraw(Graphics g) { // 화면 그리기
 		g.drawImage(Background, 0, 0, null);
 		if (isScreen1) {
-			g.drawImage(Map, 360, 0, 920, 720, null);
+			// g.drawImage(Map, 360, 0, 920, 720, null);
 		}
 		paintComponents(g);
 		this.repaint();
--- foodmap/src/foodmap/Main.java
+++ foodmap/src/foodmap/Main.java
@@ -1,11 +1,26 @@
 package foodmap;
+import foodmap.Resturant;
+import java.util.Vector;
 
 public class Main {
 	
-	public static final int SCREEN_WIDTH = 1280;
-	public static final int SCREEN_HEIGHT = 720;
+	public static final int SCREEN_WIDTH = 1060;
+	public static final int SCREEN_HEIGHT = 500;
 	
 	public static void main(String[] args) {
 		new Foodmap();
+		
+		Resturant GeoSung = new Resturant(1, "거성", "09:00~18:00", "010-0000-0000", "매주 월요일", "충절로1600");//객체 찍기
+		GeoSung.SetMenu("사진 url");//메뉴사진등록
+		GeoSung.SetPhoto("사진 url");//가게사진 등록
+		//Resturant chowon = new Resturant(2, "초원", "09:00~18:00", "010-0000-0000", "매주 월요일", "충절로1600");
+		//GeoSung.SetMenu("사진 url");
+		//GeoSung.SetPhoto("사진 url");
+		
+		Vector <Resturant> V = new Vector <Resturant> ();
+		V.add(GeoSung);//객체 만들었으면 벡터에 추가하기
+		//V.add(chowon);
+		//V.add(soosin);
+		
 	}
 }
