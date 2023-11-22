8ea7e1ecab271602d155b1b6b6f4c8a70513e473
jugak <150978124+jugak@users.noreply.github.com>
2023-11-22 20:42:02 +0900

    Category.java

--- foodmap/src/foodmap/Category.java
+++ foodmap/src/foodmap/Category.java
@@ -12,28 +12,51 @@ import javax.swing.JLabel;
 
 
 public class Category extends JPanel{
-	//이미지는 변경 해야함 
-	private ImageIcon KfoodBasicbutton = new ImageIcon(Main.class.getResource(""));
+	//이미지는 추가 해야함 
+	private ImageIcon KfoodBasiclabel = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon CfoodBasiclabel = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon JfoodBasiclabel = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon WfoodBasiclabel = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon SfoodBasiclabel = new ImageIcon(Main.class.getResource(""));
 	private ImageIcon Wangbasicbutton = new ImageIcon(Main.class.getResource(""));
 	private ImageIcon Jeulgyeobasicbutton = new ImageIcon(Main.class.getResource(""));
 	private ImageIcon Hansotbasicbutton = new ImageIcon(Main.class.getResource(""));
 	private ImageIcon Sseonybasicbutton = new ImageIcon(Main.class.getResource(""));
 	private ImageIcon Chowonbasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Yongbasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Soosinbasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Bohwabasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Wellbasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Yogitbasicbutton = new ImageIcon(Main.class.getResource(""));
+	private ImageIcon Sinjeonbasicbutton = new ImageIcon(Main.class.getResource(""));
 	
 	
-	private JLabel Kfoodlabel = new JLabel(KfoodBasicbutton); // 한식 카테고리 라벨
+	private JLabel Kfoodlabel = new JLabel(KfoodBasiclabel); // 한식 카테고리 라벨
 	private JButton Wangbutton = new JButton(Wangbasicbutton); // 한식 식당 버튼들
 	private JButton Jeulgyeobutton = new JButton(Jeulgyeobasicbutton);
 	private JButton Hansotbutton = new JButton(Hansotbasicbutton);
 	private JButton Sseonybutton = new JButton(Sseonybasicbutton);
-	private JButton Chowonbutton = new JButton(Chowonbasicbutton);
+	
+	private JLabel Cfoodlabel = new JLabel(CfoodBasiclabel); // 중식 카테고리 라벨
+	private JButton Chowonbutton = new JButton(Chowonbasicbutton); // 중식 식당 버튼들
+	private JButton Yongbutton = new JButton(Yongbasicbutton);
+	private JButton Soosinbutton = new JButton(Soosinbasicbutton);
+	private JButton Bohwabutton = new JButton(Bohwabasicbutton);
+	
+	private JLabel Jfoodlabel = new JLabel(JfoodBasiclabel); // 중식 카테고리 라벨
+	private JButton Wellbutton = new JButton(Wellbasicbutton); // 중식 식당 버튼들
+	
+	private JLabel Wfoodlabel = new JLabel(WfoodBasiclabel); // 중식 카테고리 라벨
+	private JButton Yogitbutton = new JButton(Yogitbasicbutton); // 중식 식당 버튼들
+	
+	private JLabel Sfoodlabel = new JLabel(SfoodBasiclabel); // 중식 카테고리 라벨
+	private JButton Sinjeonbutton = new JButton(Sinjeonbasicbutton); // 중식 식당 버튼들
 	
 	
-	void KfoodCategory() {
+	
+	void KfoodCategory() {//한식 카테고리 음식점 버튼
 		Kfoodlabel.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
-		
-		
-		
+
 		Wangbutton.setBounds(0, 100, 360, 100); // 버튼 위치 및 사이즈
 		Wangbutton.addMouseListener(new MouseAdapter() { // 재정의
 			@Override
@@ -42,16 +65,13 @@ public class Category extends JPanel{
 			}
 		});
 
-		
-		
 		Jeulgyeobutton.setBounds(0, 200, 360, 100); // 버튼 위치 및 사이즈
 		Jeulgyeobutton.addMouseListener(new MouseAdapter() { // 재정의
 			@Override
 			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
 				// 즐겨먹기 버튼 눌렀을 때
 			}
-		});
-		
+		});		
 		
 		Hansotbutton.setBounds(0, 300, 360, 100); // 버튼 위치 및 사이즈
 		Hansotbutton.addMouseListener(new MouseAdapter() { // 재정의
@@ -69,16 +89,86 @@ public class Category extends JPanel{
 			}
 		});
 		
-		Chowonbutton.setBounds(0, 500, 360, 100); // 버튼 위치 및 사이즈
+		}
+	
+	void CfoodCategory() {// 중식 카테고리 음식점 버튼
+		Cfoodlabel.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
+
+		Chowonbutton.setBounds(0, 100, 360, 100); // 버튼 위치 및 사이즈
 		Chowonbutton.addMouseListener(new MouseAdapter() { // 재정의
 			@Override
 			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
 				// 초원 버튼 눌렀을 때
 			}
 		});
-	}
+
+		Yongbutton.setBounds(0, 200, 360, 100); // 버튼 위치 및 사이즈
+		Yongbutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 용짬뽕 버튼 눌렀을 때
+			}
+		});		
+		
+		Soosinbutton.setBounds(0, 300, 360, 100); // 버튼 위치 및 사이즈
+		Soosinbutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 수신 버튼 눌렀을 때
+			}
+		});
+		
+		Bohwabutton.setBounds(0, 400, 360, 100); // 버튼 위치 및 사이즈
+		Bohwabutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 보화 버튼 눌렀을 때
+			}
+		});
+		
+		}
 	
-	}
+	void JfoodCategory() {//일식 카테고리 음식점 버튼
+		Jfoodlabel.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
+
+		Wellbutton.setBounds(0, 100, 360, 100); // 버튼 위치 및 사이즈
+		Wellbutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 초원 버튼 눌렀을 때
+			}
+		});
+
+		
+		}
 	
+	void WfoodCategory() {//양식 카테고리 음식점 버튼
+		Wfoodlabel.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
+
+		Yogitbutton.setBounds(0, 100, 360, 100); // 버튼 위치 및 사이즈
+		Yogitbutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 초원 버튼 눌렀을 때
+			}
+		});
+
+		
+		}
+		
+	void SfoodCategory() {//분식 카테고리 음식점 버튼 
+		Sfoodlabel.setBounds(0, 0, 360, 100); // 버튼 위치 및 사이즈
+
+		Sinjeonbutton.setBounds(0, 100, 360, 100); // 버튼 위치 및 사이즈
+		Sinjeonbutton.addMouseListener(new MouseAdapter() { // 재정의
+			@Override
+			public void mousePressed(MouseEvent e) { // 마우스 눌렀을 때
+				// 초원 버튼 눌렀을 때
+			}
+		});
+
+		
+		}
 
 }
+	
