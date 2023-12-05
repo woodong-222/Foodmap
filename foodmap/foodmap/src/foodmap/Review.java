21acf64cbfa63014ee39b6c41597d6431295911e
jugak <150978124+jugak@users.noreply.github.com>
2023-12-06 03:32:38 +0900

    Update Review.java

--- foodmap/foodmap/src/foodmap/Review.java
+++ foodmap/foodmap/src/foodmap/Review.java
@@ -7,42 +7,44 @@ import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
+import javax.swing.JComboBox;
 import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Color;
 
 public class Review extends JDialog {
 
-    private final JPanel contentPanel = new JPanel();
-    private JTextField textField;
-    private JTextField textField_1;
-    public JLabel contentLabel; // 여기에 contentLabel 선언
+	private final JPanel contentPanel = new JPanel();
+	private Restaurant restaurant; // Restaurant 객체에 대한 참조 추가
+	private JTextField textField;
+	private JTextField textField_1;
+	private JComboBox<Double> starComboBox;
+	private ShowRestaurant showRestaurantPanel;
 
-    public Review() {
-    	
-    	contentLabel = new JLabel(); // contentLabel 초기화
-        contentLabel.setBounds(0, 222, 360, 420); // 적절한 크기 및 위치 설정
-        getContentPane().add(contentLabel);
-        
+	public Review(Restaurant restaurant, ShowRestaurant showRestaurantPanel) {
+		this.restaurant = restaurant;
+		this.showRestaurantPanel = showRestaurantPanel;
+    	setForeground(new Color(255, 255, 255));
+    	setIconImage(Toolkit.getDefaultToolkit().getImage(Review.class.getResource("/images/logo.png")));
         setBounds(100, 100, 450, 218);
+        setTitle("리뷰 작성");
+		setLocationRelativeTo(null); // 컴퓨터 정중앙
         getContentPane().setLayout(null);
         contentPanel.setLayout(null);
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
 
-        // 스피너 설정 부분
-        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(0.0, 0.0, 5.0, 0.1);
-        JSpinner spinner = new JSpinner(spinnerModel);
-        spinner.setBounds(365, 36, 60, 22);
-        getContentPane().add(spinner);
+        //콤보박스
+		Double[] starOptions = { 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 };
+		starComboBox = new JComboBox<>(starOptions);
+		starComboBox.setBounds(365, 36, 60, 22);
+		getContentPane().add(starComboBox);
         
         JLabel lblNewLabel = new JLabel("별점");
         lblNewLabel.setBounds(303, 39, 50, 15);
@@ -76,67 +78,36 @@ public class Review extends JDialog {
 
         // "등록" 버튼에 ActionListener 추가
         btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // "등록" 버튼이 클릭되면 파일에 내용 저장
-                saveToFile();
-                displayFileContent();
-                dispose();
-            }
-        });
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveReview(); // 리뷰 저장 메소드 호출
+			}
+		});
 
-        // "취소" 버튼에 ActionListener 추가
-        btnNewButton_1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // "취소" 버튼이 클릭되면 다이얼로그를 닫음
-                dispose();
-            }
-        });
-    }
+		// "취소" 버튼에 ActionListener 추가
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// "취소" 버튼이 클릭되면 다이얼로그를 닫음
+				dispose();
+				repaint();
+			}
+		});
+	}
 
-    // 파일에 내용 저장하는 메서드
-    private void saveToFile() {
-        // 파일 경로 및 파일명 설정 (본인의 프로젝트에 맞게 수정)
-    	String filePath = "C:/Users/juhoj/Desktop/허명철 시발련/Foodmap/foodmap/src/reviews/review.txt";
-    	// 텍스트 필드의 내용 가져오기
-        String text1 = textField.getText();
-        String text2 = textField_1.getText();
+	private void saveReview() {
+		String username = textField.getText();
+	    String reviewContent = textField_1.getText();
+	    Double selectedStar = (Double) starComboBox.getSelectedItem();
 
-        // 파일 쓰기 작업
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            writer.write(text1 + " : " + text2);
-            writer.newLine(); // 개행 문자 추가
-            System.out.println("파일에 내용 저장 완료!");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.err.println("파일 저장 중 오류 발생");
-        }
-    }
-    
-    public void displayFileContent() {
-        // 파일 경로 및 파일명 설정 (본인의 프로젝트에 맞게 수정)
-        String filePath = "C:/Users/juhoj/Desktop/허명철 시발련/Foodmap/foodmap/src/reviews/review.txt";
+	    // 사용자 이름, 리뷰 내용, 선택된 별점이 모두 유효한지 확인합니다.
+	    if (!username.isEmpty() && !reviewContent.isEmpty() && selectedStar != null) {
+	        // Restaurant 객체에 리뷰를 추가합니다.
+	        restaurant.addReview(username, reviewContent, selectedStar);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            StringBuilder content = new StringBuilder();
-            String line;
-            
-            // 파일에서 각 줄을 읽어와 StringBuilder에 추가
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-
-            // StringBuilder에 저장된 내용을 JLabel에 표시
-            // 여기서는 contentLabel이라는 JLabel을 가정합니다.
-            contentLabel.setText("<html>" + content.toString() + "</html>");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.err.println("파일 읽기 중 오류 발생");
-        }
-    }
-    
-    public JLabel getContentLabel() {
-        return contentLabel;
-    }
-    
+            showRestaurantPanel.DisplayReviewsAndAverageStars();
+	        // 다이얼로그 창을 닫습니다.
+	        dispose();
+	    }
+	}
 }
+
